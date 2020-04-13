55095f7f32f2430c40ba45fe5388a76c02f8130f
Daniel Cooper <danielcooper@daniels-air.home>
2020-04-13 17:57:31 +0100

    removes animations for testing

--- src/components/Index/Mockup/Slideshow.js
+++ src/components/Index/Mockup/Slideshow.js
@@ -2,37 +2,37 @@ import React from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import styled from "styled-components"
 
-const variants = {
-  enter: {
-    opacity: 0,
-  },
-  center: {
-    opacity: 1,
-  },
-  exit: {
-    opacity: 0,
-  },
-}
+// const variants = {
+//   enter: {
+//     opacity: 0,
+//   },
+//   center: {
+//     opacity: 1,
+//   },
+//   exit: {
+//     opacity: 0,
+//   },
+// }
 
 const Slideshow = ({ image }) => (
-  <AnimatePresence initial={false}>
-    <StyledSlideshow
-      key={image}
-      variants={variants}
-      initial="enter"
-      animate="center"
-      exit="exit"
-      transition={{
-        x: { type: "spring", stiffness: 300, damping: 200 },
-        duration: 0.5,
-      }}
-    >
-      <img src={image} alt="Website screenshot" />
-    </StyledSlideshow>
-  </AnimatePresence>
+  // <AnimatePresence initial={false}>
+  <StyledSlideshow
+  // key={image}
+  // variants={variants}
+  // initial="enter"
+  // animate="center"
+  // exit="exit"
+  // transition={{
+  //   x: { type: "spring", stiffness: 300, damping: 200 },
+  //   duration: 0.5,
+  // }}
+  >
+    <img src={image} alt="Website screenshot" />
+  </StyledSlideshow>
+  // </AnimatePresence>
 )
 
-const StyledSlideshow = styled(motion.div)`
+const StyledSlideshow = styled.div`
   position: absolute;
   height: 61.3%;
   width: 92.2%;
